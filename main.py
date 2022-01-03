3af24e582096f8126ed0c35f858e2313cfbdc99c
Aadit Vyas <aaditvyas@gmail.com>
2022-01-03 16:36:50 -0500

    adds a function to use an nltk dictionary to check if a word is in the english dictionary

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import argparse
+from nltk.corpus import words
 
 
+# function to handle parsing arguments from the commandline
 def parse_arguments():
     parser = argparse.ArgumentParser(description='app to suggest words for the nyt spelling bee game.')
     parser.add_argument('all_letters', type=str, help='set of all letters')
@@ -9,8 +11,16 @@ def parse_arguments():
     return args
 
 
+# prepare word dictionary
+def get_dictionary():
+    eng_dict = set(words.words())
+    return eng_dict
+
+
 def main():
     args = parse_arguments()
+    eng_dict = get_dictionary()
+
 
 if __name__ == "__main__":
     main()
