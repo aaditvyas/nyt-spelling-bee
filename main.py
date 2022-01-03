87709e438eb407d58212e8de1f8256c310327ae1
Aadit Vyas <aaditvyas@gmail.com>
2022-01-03 17:03:24 -0500

    adds ability to suggest a word using the given set of letters

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+from itertools import permutations
 from nltk.corpus import words
 
 
@@ -17,9 +18,32 @@ def get_dictionary():
     return eng_dict
 
 
+# given a set of letters, returns an english word from its permutations
+def generate_word(letters, required_letter, checked_words, eng_dict):
+    # start with the longest words then go smaller
+    for word_length in range(len(letters)+1, 1, -1):
+        # returns first eng word encountered that has not been seen and contains the required letter
+        for potential_word_letters in permutations(letters, word_length):
+            potential_word = ''.join(potential_word_letters)
+            if potential_word in checked_words or required_letter not in potential_word:
+                continue
+            elif potential_word in eng_dict:
+                checked_words.add(potential_word)
+                return potential_word, checked_words
+    return -1, checked_words
+
+
 def main():
+    checked_words = set()
     args = parse_arguments()
     eng_dict = get_dictionary()
+    potential_word = generate_word(args.all_letters, args.required_letter, checked_words, eng_dict)
+
+    print(potential_word)
+
+    # first pass will be using 0 repeat letters
+
+    # second pass will be using 1 repeat letter for each letter
 
 
 if __name__ == "__main__":
